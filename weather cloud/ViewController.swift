64b6c062d3318e2f7f4e91bae5d3401eefc4da61
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-09-02 14:30:39 +0300

    Initial Commit

---
+++ weather cloud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather cloud
+//
+//  Created by Abdirizak Hassan on 9/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
